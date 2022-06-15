b36c9a54f7d7bd53d1e88e631a9981911ed925b1
sm <ksm1538@naver.com>
2022-06-15 23:19:40 +0900

    spring security : JWT 임시토큰으로 인증,인가해보기

--- security1/src/main/java/com/cos/security1/controller/RestControllerAPI.java
+++ security1/src/main/java/com/cos/security1/controller/RestControllerAPI.java
@@ -1,12 +1,18 @@
 package com.cos.security1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerAPI {
     @GetMapping("home")
-    public String home(){
+    public String Home(){
         return "<h1>home</h1>";
+    }
+
+    @PostMapping("token")
+    public String Token(){
+        return "<h1>token</h1>";
     }
 }
