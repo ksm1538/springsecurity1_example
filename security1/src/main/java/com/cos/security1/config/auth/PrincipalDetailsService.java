f57f94604c093911fa2a6abd6a43305eed8745f0
ksm15 <ksm1538@naver.com>
2022-06-18 17:02:10 +0900

    JWT 프로젝트와 분리( 해당 프로젝트는 security 로그인 및 Oauth2 )

--- security1/src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
+++ security1/src/main/java/com/cos/security1/config/auth/PrincipalDetailsService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
 
+    // spring data jpa
     @Autowired
     private UserRepository userRepository;
 
